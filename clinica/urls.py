fcf20f2c79d5d300c570eecee00faa976084c2d8
William Santos <Willsantos.edf@gmail.com>
2023-02-04 16:50:00 -0300

    Criando paginas de atualização.

--- clinica/urls.py
+++ clinica/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import PaginaInicial, PacienteCreate, ConsultaCreate, MedicoCreate, CategoriaCreate
+from .views import PacienteUpdate, ConsultaUpdate, MedicoUpdate, CategoriaUpdate
 
 
 urlpatterns = [
@@ -8,5 +9,10 @@ urlpatterns = [
     path('clinica/consulta/', ConsultaCreate.as_view(), name='cadastrar-consulta'),
     path('clinica/medico/', MedicoCreate.as_view(), name='cadastrar-medico'),
     path('clinica/categoria/', CategoriaCreate.as_view(), name='cadastrar-categoria'),
+
+    path('editar/paciente/<int:pk>/',PacienteUpdate.as_view(),name='editar-paciente'),
+    path('editar/consulta/<int:pk>/',ConsultaUpdate.as_view(),name='editar-consulta'),
+    path('editar/medico/<int:pk>/',MedicoUpdate.as_view(),name='editar-medico'),
+    path('editar/categoria/<int:pk>/',CategoriaUpdate.as_view(),name='editar-categoria'),
     
 ]
--- clinica/views.py
+++ clinica/views.py
@@ -1,12 +1,12 @@
 
 from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView
 
 from .models import Paciente, Consulta, Categoria, Medico
 
 from django.urls import reverse_lazy
 
-
+################ Create ##################
 
 class PaginaInicial(TemplateView):
     template_name = "clinica/index.html"
@@ -30,6 +30,32 @@ class PacienteCreate(CreateView):
     success_url = reverse_lazy('inicio')
 
 class ConsultaCreate(CreateView):
+    model = Consulta
+    fields = '__all__'
+    template_name = 'clinica/form.html'
+    success_url = reverse_lazy('inicio')
+
+    ################ Update ##################
+
+class CategoriaUpdate(UpdateView):
+    model = Categoria
+    fields = '__all__'
+    template_name = 'clinica/form.html'
+    success_url = reverse_lazy('inicio')
+
+class MedicoUpdate(UpdateView):
+    model = Medico
+    fields = '__all__'
+    template_name = 'clinica/form.html'
+    success_url = reverse_lazy('inicio')
+
+class PacienteUpdate(UpdateView):
+    model = Paciente
+    fields = '__all__'
+    template_name = 'clinica/form.html'
+    success_url = reverse_lazy('inicio')
+
+class ConsultaUpdate(UpdateView):
     model = Consulta
     fields = '__all__'
     template_name = 'clinica/form.html'
